433cdb00997e61243baf6018e6b26568490c4f9c
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-07-31 15:25:22 -0400

    first iteration of the site

---
+++ app/skills.py
@@ -0,0 +1,17 @@
+skills = [
+        ('Algorithms', 2),
+        ('Machine Learning', 1),
+        ('Information Retrieval', 1),
+        ('Design', 2),
+        ('Python', 3),
+        ('HTML', 2),
+        ('CSS', 2),
+        ('less', 2),
+        ('Wordpress', 2),
+        ('Flask', 2),
+        ('Databases', 1),
+        ('Parallel Programming', 1),
+        ('Vim', 2)
+]
+
+skills = sorted(skills)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from app import app
 from flask import render_template
+from skills import skills
 
-pages = ['about', 'projects', 'resume', 'blog']
+pages = ['intro', 'projects', 'skills']
 
 def render(page_to_render):
-    return render_template(page_to_render + '.html', pages=pages, current_page=page_to_render)
+    return render_template(page_to_render + '.html', pages=pages, current_page=page_to_render, skills=skills)
 
 @app.route('/')
 @app.route('/index')
