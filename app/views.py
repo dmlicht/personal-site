8e6eabaa5a60a2bc59bf1d105e5a576539bde092
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-07-31 12:05:00 -0400

    removed routes to pages that were added to home

--- app/views.py
+++ app/views.py
@@ -1,25 +1,15 @@
 from app import app
 from flask import render_template
 
-
-pages = ['about', 'resume', 'projects', 'blog']
+pages = ['about', 'projects', 'resume', 'blog']
 
 def render(page_to_render):
     return render_template(page_to_render + '.html', pages=pages, current_page=page_to_render)
 
 @app.route('/')
 @app.route('/index')
-@app.route('/about')
 def index():
-    return render('about')
-
-@app.route('/resume')
-def resume():
-    return render('resume')
-
-@app.route('/projects')
-def projects():
-    return render('projects')
+    return render('base')
 
 @app.route('/blog')
 def blog():
